228181659708846d612c9262dda114250b617b21
Adéliton Fernandes <adeliton@live.com>
2024-01-11 19:02:49 -0400

    Atualização aula 11-01

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,61 @@
-import { Menu, Notifications } from '@mui/icons-material'
-import { AppBar, Box, CssBaseline, Drawer, IconButton, Toolbar, Typography } from '@mui/material'
+import { ArrowCircleLeft, ArrowLeft, HomeMax, HomeOutlined, Menu, Notifications, Place, Restaurant, RestaurantMenu } from '@mui/icons-material'
+import { AppBar, Box, Button, CssBaseline, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
   const [drawer, setDrawer] = useState(false)
+  const navigate = useNavigate()
+
+  const itensDoMenu = [
+    {
+        texto: 'Página Inicial',
+        icone: <HomeOutlined/>,
+        destino: '/home'
+    },
+    {
+        texto: 'Meu Restaurante',
+        icone: <Restaurant/>,
+        destino: '/meu-restaurante'
+    },
+    {
+        texto: 'Meus Ítens',
+        icone: <RestaurantMenu/>,
+        destino: '/meus-itens'
+    },
+    {
+        texto: 'Restaurantes',
+        icone: <Place/>,
+        destino: '/restaurantes'
+    },
+    {
+        texto: 'Sair',
+        icone: <ArrowCircleLeft/>,
+        destino: '/sair'
+    }
+  ];
+
   return (
     <>
         <Drawer open={drawer} onClose={() => {setDrawer(false)}}>
-            Olá Mundo
+            <Box sx={{ width: 250 }}>
+                <List>
+                    {
+                        itensDoMenu.map((itemDoMenu) => (
+                            <ListItem>
+                                <ListItemButton onClick={() => {
+                                    navigate(itemDoMenu.destino)
+                                }}>
+                                    <ListItemIcon>
+                                        {itemDoMenu.icone}
+                                    </ListItemIcon>
+                                    <ListItemText primary={itemDoMenu.texto}/>
+                                </ListItemButton>
+                            </ListItem>
+                        ))
+                    }
+                </List>
+            </Box>
         </Drawer>
         <Box sx={{display: 'flex'}}>
             <CssBaseline/>
