6a31c93d2240e3418fc75014979f0d89a4fa29b9
Adéliton Fernandes <adeliton@live.com>
2024-01-18 21:19:04 -0400

    Aula 18-01

--- src/pages/ListarItens.jsx
+++ src/pages/ListarItens.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MenuLateral from '../components/MenuLateral'
-import { Fab } from '@mui/material'
+import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Container, Fab, Grid } from '@mui/material'
 import { Add } from '@mui/icons-material'
 import { useNavigate, useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const ListarItens = () => {
     const navigate = useNavigate()
     const { restaurante_id } = useParams()
+    const [itens, setItens] = useState([])
+
+    const obterDados = async () => {
+        const response = await axios.get("http://localhost:3001/itens/" + restaurante_id, {
+            headers: {
+                "Authorization": localStorage.getItem("token")
+            }
+        })
+        setItens(response.data)
+    }
+
+    const excluirItem = async (id) => {
+        try {
+            await axios.delete(`http://localhost:3001/itens/${restaurante_id}/${id}`, {
+                headers: {
+                    "Authorization": localStorage.getItem("token")
+                }
+            })
+            alert("Item excluido com sucesso")
+            obterDados()
+        } catch (e) {
+            alert("Erro ao excluir item")
+        }
+    }
+
+    useEffect(() => {
+        obterDados()
+    }, [])
 
   return (
     <>
@@ -20,6 +49,38 @@ const ListarItens = () => {
         }}>
             <Add/>
         </Fab>
+        <Container sx={{ mt: 10 }}>
+            <Grid container spacing={2}>
+                { itens.map((item) => 
+                    (<Grid item xs={12} md={3}>
+                    <Card sx={{ height: '460px' }}>
+                        
+                        <CardMedia
+                            component="img"
+                            height="200"
+                            image={ "http://localhost:3001/" + item.foto?.replace("public", "") }
+                        />
+                        <CardHeader
+                            title={ item.nome }
+                            subheader={ item.descricao }
+                        />
+                        <CardContent>
+                            R$ { item.valor } <br/>
+                            Quantidade em estoque: { item.quantidade_estoque }
+                        </CardContent>
+                        <CardActions>
+                            <Button variant='contained' onClick={() => {}}>
+                                EDITAR 
+                            </Button>
+                            <Button variant='contained' color='error' onClick={() => { excluirItem(item.id) }}>
+                                EXCLUIR
+                            </Button>
+                        </CardActions>
+                    </Card>
+                    </Grid>)
+                ) }
+            </Grid>
+        </Container>
     </>
   )
 }
